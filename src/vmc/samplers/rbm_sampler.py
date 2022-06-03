RBM, something wrong in gradient?

src/vmc/samplers/rbm_sampler.py
@@ -96,6 +96,7 @@ class BaseRBMVMC:
 
             for _ in range(nsamples):
                 state  = self.step(state, seed)
+                #print("State positions: ", state.positions)
                 energies.append(self._locE_fn(state.positions))
                 grad_a.append(self._grad_a_fn(state.positions))
                 grad_b.append(self._grad_b_fn(state.positions))
@@ -105,15 +106,21 @@ class BaseRBMVMC:
             grad_a = np.array(grad_a)
             grad_b = np.array(grad_b)
             grad_W = np.array(grad_W)
+            shape_a = grad_a.shape
+            shape_b = grad_b.shape
+            shape_W = grad_W.shape
 
             expect_energy = np.mean(energies)
             expect_grad_a = np.array(np.mean(grad_a, axis=0))
             expect_grad_b = np.array(np.mean(grad_b, axis=0))
             expect_grad_W = np.array(np.mean(grad_W, axis=0))
-            expect_grad_a_E = np.mean(np.multiply(energies, grad_a), axis=0)
+            #print("Shape E*a: ", (energies*grad_a).shape)
+            expect_grad_a_E = np.mean(energies.reshape(nsamples, 1, 1)*grad_a, axis=0)
             expect_grad_b_E = np.mean(energies.reshape(nsamples, 1)*grad_b, axis=0)
-            expect_grad_W_E = np.mean(energies*grad_W, axis=0)
+            expect_grad_W_E = np.mean(energies.reshape(nsamples, 1, 1, 1)*grad_W, axis=0)
 
+
+            """
             print("Shape grad a: ", expect_grad_a.shape)
             print("Shape grad b: ", expect_grad_b.shape)
             print("Shape grad W: ", expect_grad_W.shape)
@@ -122,7 +129,7 @@ class BaseRBMVMC:
             print("Shape bE: ", expect_grad_b_E.shape)
             print("Shape WE: ", expect_grad_W_E.shape)
             print("Shape energies: ", energies.shape)
-
+            """
 
             gradient_a = 2 * (expect_grad_a_E - expect_grad_a * expect_energy)
             gradient_b = 2 * (expect_grad_b_E - expect_grad_b * expect_energy)
@@ -130,12 +137,12 @@ class BaseRBMVMC:
 
             self.update_parameters(gradient_a, gradient_b, gradient_W, eta=eta)
 
-            printf(f"At iteration {i}: Energy={expect_energy}.")
+            print(f"At iteration {i}: Energy={expect_energy}.")
             training_energies[i] = expect_energy
 
         return training_energies
 
-    def update_parameters(self, grad_a, grad_b, grad_W, eta=0.01):
+    def update_parameters(self, grad_a, grad_b, grad_W, eta=0.1):
         """
         Updates the biases and weights of the RBM
 

src/vmc/systems/rbm.py
@@ -6,7 +6,7 @@ from . import BaseRBM
 
 
 class RBMWF(BaseRBM):
-    def __init__(self, n_particles, dim, n_hidden, rng=None, seed=0, scale=1.0, loc=0.0, omega=1.0, hard_sphere_diameter=0.00433):
+    def __init__(self, n_particles, dim, n_hidden, rng=None, seed=0, scale=0.1, loc=0.0, omega=1.0, hard_sphere_diameter=0.00433):
         """
         Initiates biases and weights of one visible and one hidden layer,
         in addition to making some variables.
@@ -41,7 +41,7 @@ class RBMWF(BaseRBM):
         ---------
         A scalar value representing the NQS wave function.
         """
-        gaussian = np.sum((r-self._a)/(4*self._sigma2))
+        gaussian = np.sum((r-self._a)**2/(4*self._sigma2))
         Q = self._Q(r)
 
         return -gaussian + np.sum(np.log(Q))
@@ -102,6 +102,8 @@ class RBMWF(BaseRBM):
         grad = self._gradient(r)
         grad2 = self._log_laplacian(r)
         gradient_term = np.sum(grad*grad)
+        print("Grad 2: ", grad2)
+        print("Gradient term: ", gradient_term)
         return gradient_term + grad2
 
     def _kinetic_energy(self, r):
@@ -117,6 +119,7 @@ class RBMWF(BaseRBM):
         Scalar
         """
         kinetic_energy = -0.5*self._laplacian(r)
+        print("Kinetic energy: ", kinetic_energy)
         return kinetic_energy
 
     def _correlation(self, r):
@@ -151,6 +154,7 @@ class RBMWF(BaseRBM):
         """
         Vint = self._correlation(r)
         Vtrap = 0.5 * self._omega2 * np.sum(r*r)
+        print("Potential energy: ", Vint+Vtrap)
         return Vtrap + Vint
 
 
@@ -167,7 +171,7 @@ class RBMWF(BaseRBM):
         An np.ndarray(shape=(n_hidden)) value corresponding to
                     (1 + e^{b+rW/s^2})
         """
-        Q = 1 + np.exp(self._b + np.einsum("ij,ijk->k", r, self._W)/self._sigma2)
+        Q = np.exp(self._b + np.einsum("ij,ijk->k", r, self._W)/self._sigma2)
         return Q
 
     def _denominator(self, r):